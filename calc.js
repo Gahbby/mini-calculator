3fdbbb33690ad5abcc704d9fb0137836aeaca6bf
Adebayo Bukunmi Gabriel <gabrielopemipo1@gmail.com>
2025-05-05 14:44:53 +0100

    mini calculator

---
+++ calc.js
@@ -0,0 +1,11 @@
+function displayNumber(value){
+    console.log(value);
+    userInput.value += value
+}
+
+function evaluateValues(){
+    let displayedValues = userInput.value
+    let result = eval(displayedValues)
+    answer.innerHTML = `<h3> Answer = ${result} <h3>`
+    userInput.value = result
+}
